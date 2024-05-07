3f412f786ed16213af20b2202e9668c44d570dbd
Víctor García <gc.victor@gmail.com>
2024-05-07 14:50:47 +0200

    fix: deploy commands

--- .query/tasks/deploy.sh
+++ .query/tasks/deploy.sh
@@ -4,29 +4,29 @@ export PROD=true
 
 deploy_bundle() {
     start=$(date +%s%N)
-    ../query/target/debug/query task bundle
+    pnpm query task bundle
     end=$(date +%s%N)
     echo "Bundled: $(((end - start) / 1000000))ms"
 }
 
 deploy_tailwind() {
     start=$(date +%s%N)
-    ../query/target/debug/query task tailwind
+    pnpm query task tailwind
     end=$(date +%s%N)
     echo "Styles created: $(((end - start) / 1000000))ms"
 }
 
 deploy_asset() {
     start=$(date +%s%N)
-    ../query/target/debug/query asset dist &
-    ../query/target/debug/query asset public
+    pnpm query asset dist &
+    pnpm query asset public
     end=$(date +%s%N)
     echo "Assets updated: $(((end - start) / 1000000))ms"
 }
 
 deploy_function() {
     start=$(date +%s%N)
-    ../query/target/debug/query function
+    pnpm query function
     end=$(date +%s%N)
     echo "Functions updated: $(((end - start) / 1000000))ms"
 }
